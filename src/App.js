6edd5108b9715024802720932c520a6cb1b26739
William Boyden <59125609+willboyden@users.noreply.github.com>
2020-03-31 09:48:29 -0400

    trying out github gui for branching new features, pivot table with hooks working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import MaMap from "./components/maMap";
-import { useRoutes, A } from "hookrouter";
+import { useRoutes, A, useRedirect } from "hookrouter";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./StyleSheets/navBar.css";
@@ -10,18 +10,105 @@ import Home from "./components/Home";
 import Technology from "./components/Technology";
 import CommingSoon from "./components/CommingSoon";
 import Contact from "./components/Contact";
+import PivotTableUI from "./components/PivotTableWithUI";
+import PrivacyPolicy from "./components/PrivacyPolicy";
+import { DataProvider } from "./components/DataContext";
+import { UserProvider } from "./components/UserContext";
+import PivotTableWrapper from "./components/PivotTableWrapper";
 //import "./main.scss"; // webpack must be configured to do this
 
+import StoreFetchDataParallel from "./components/FetchDataParallel";
+import PivotTableWithUI from "./components/PivotTableWithUI";
+
 export default function App() {
+  const [dateParam, setDateParam] = useState({});
+  const [sidenavWidth, SetSidenavWidth] = useState("250px");
+  const [mainMarginLeft, SetMainMrginLeft] = useState("250px");
+  const user = { name: "Tania", loggedIn: true };
+  //TestC();
+
   //1 react hook with method to set data and var to hold data
   const [venue, setVenue] = useState({
     name: "Big Night Live",
     dataSource: "ticketmaster"
   });
+  const data = async () => await StoreFetchDataParallel(urls);
+  //console.log(data);
 
-  const [dateParam, setDateParam] = useState({});
-  const [sidenavWidth, SetSidenavWidth] = useState("250px");
-  const [mainMarginLeft, SetMainMrginLeft] = useState("250px");
+  //  console.log(data);
+  const routes = {
+    "/": () => <Home />,
+    "/eventfinder": () => <EventFinder />,
+    "/exploredata": () => (
+      <PivotTableWrapper
+        data={data}
+        pivotState={{
+          mode: "demo",
+          filename: "Event Data Explorer",
+          // textarea: 'Sample Dataset: Tips',
+          pivotState: {
+            data: data,
+            // rows: ['Payer Gender'],
+            //  cols: ['Party Size'],
+            //  aggregatorName: 'Sum over Sum',
+            //   vals: ['Tip', 'Total Bill'],
+            rendererName: "Grouped Column Chart",
+            // sorters: {
+            //   Meal: sortAs(['Lunch', 'Dinner']),
+            //   'Day of Week': sortAs(['Thursday', 'Friday', 'Saturday', 'Sunday']),
+            // },
+            plotlyOptions: { width: 900, height: 500 },
+            plotlyConfig: {},
+            tableOptions: {
+              clickCallback: function(e, value, filters, pivotData) {
+                var names = [];
+                pivotData.forEachMatchingRecord(filters, function(record) {
+                  names.push(record.Meal);
+                });
+                alert(names.join("\n"));
+              }
+            }
+          }
+        }}
+      />
+    ),
+    "/eventcalendar": () => <EventCalendar venue={venue} />,
+    "/venuemap": () => <MaMap />,
+    "/technology": () => <Technology />,
+    "/contact": () => <Contact />,
+    "/comingsoon": () => <CommingSoon />,
+    "/privacypolicy": () => <PrivacyPolicy />,
+    "/*": () => <PrivacyPolicy />
+  };
+
+  require("dotenv").config();
+  //StoreFetchDataParallel(urls);
+  //
+  var urls = [
+    {
+      urlpath: process.env.domain + "/api/tblstubhubvenue/",
+      varname: "tblstubhubvenue"
+    },
+    {
+      urlpath: process.env.domain + "/api/tblnewticketmastervenueevent/",
+      varname: "tblnewticketmastervenueevent"
+    },
+    {
+      urlpath: process.env.domain + "/api/tblstubhubcity/",
+      varname: "tblstubhubcity"
+    },
+    {
+      urlpath: process.env.domain + "/api/tblticketmastervenue/",
+      varname: "tblticketmastervenue"
+    },
+    {
+      urlpath: process.env.domain + "/api/tblnewstubhubvenueevent/",
+      varname: "tblnewstubhubvenueevent"
+    }
+  ];
+  //const x = StoreFetchDataParallel(urls);
+
+  //x.then(y => console.log(y));
 
   function openNav() {
     SetSidenavWidth("250px");
@@ -37,41 +124,77 @@ export default function App() {
     sidenavWidth == "250px" ? closeNav() : openNav();
   }
 
-  const routes = {
-    "/": () => <Home />,
-    "/eventfinder": () => <EventFinder />,
-    "/eventcalendar": () => <EventCalendar venue={venue} />,
-    "/venuemap": () => <MaMap />,
-    "/technology": () => <Technology />,
-    "/contact": () => <Contact />,
-    "/comingsoon": () => <CommingSoon />
-  };
   const routeResult = useRoutes(routes);
-  return (
-    <div>
-      <div id="mySidenav" className="sidenav" style={{ width: sidenavWidth }}>
-        <a className="closebtn" onClick={navClick}>
-          &times;
-        </a>
-        <A href="/">Home</A>
-        <A href="/eventfinder">Event Finder</A>
-        <A href="/eventcalendar">Event Calendar</A>
-        <A href="/venuemap">Venue Map</A>
-        <A href="/technology">Technology</A>
-        <A href="/contact">Contact</A>
-        <A href="/commingsoon">Comming Soon</A>
-      </div>
 
-      <div
-        id="main"
-        className="container"
-        style={{ marginLeft: mainMarginLeft }}
-      >
-        <span style={{ cursor: "pointer" }} onClick={navClick}>
-          &#9776;
-        </span>
-        <div className="row">{routeResult}</div>
-      </div>
-    </div>
+  function setTablePivotState() {
+    return {
+      mode: "demo",
+      filename: "Event Data Explorer",
+      // textarea: 'Sample Dataset: Tips',
+      pivotState: {
+        data: { data },
+        // rows: ['Payer Gender'],
+        //  cols: ['Party Size'],
+        //  aggregatorName: 'Sum over Sum',
+        //   vals: ['Tip', 'Total Bill'],
+        rendererName: "Grouped Column Chart",
+        // sorters: {
+        //   Meal: sortAs(['Lunch', 'Dinner']),
+        //   'Day of Week': sortAs(['Thursday', 'Friday', 'Saturday', 'Sunday']),
+        // },
+        plotlyOptions: { width: 900, height: 500 },
+        plotlyConfig: {},
+        tableOptions: {
+          clickCallback: function(e, value, filters, pivotData) {
+            var names = [];
+            pivotData.forEachMatchingRecord(filters, function(record) {
+              names.push(record.Meal);
+            });
+            alert(names.join("\n"));
+          }
+        }
+      }
+    };
+  }
+
+  // console.log(data ? data : null);
+  return (
+    <DataProvider value={data}>
+      <UserProvider value={user}>
+        <div id="heyo">
+          {/* <div>{data ? data : null}</div> */}
+          <div
+            id="mySidenav"
+            className="sidenav"
+            style={{ width: sidenavWidth }}
+          >
+            <a className="closebtn" onClick={navClick}>
+              &times;
+            </a>
+            <A href="/">Home</A>
+            <A href="/eventfinder">Event Finder</A>
+            <A href="/exploredata">Explore Data</A>
+            <A href="/eventcalendar">Event Calendar</A>
+            <A href="/venuemap">Venue Map</A>
+            <A href="/technology">Technology</A>
+            <A href="/contact">Contact</A>
+            <A href="/commingsoon">Comming Soon</A>
+            <A href="/privacypolicy">Privacy Policy</A>
+          </div>
+          <div
+            id="main"
+            className="container"
+            style={{
+              marginLeft: mainMarginLeft
+            }}
+          >
+            <span style={{ cursor: "pointer" }} onClick={navClick}>
+              &#9776;
+            </span>
+            <div className="row">{routeResult}</div>
+          </div>
+        </div>
+      </UserProvider>
+    </DataProvider>
   );
 }
